5b641c46e04f5359b29fb393ae755f84102d1925
Oksenoyt <a.oksenoyt@gmail.com>
2022-06-24 10:54:24 +0300

    Initial Commit

---
+++ ColorizedApp2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ColorizedApp2
+//
+//  Created by Elenka on 24.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
