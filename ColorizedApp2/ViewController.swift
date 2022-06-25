f3e79f0dea6392e1e9180f5263df753e4d510076
Oksenoyt <a.oksenoyt@gmail.com>
2022-06-25 20:15:44 +0300

    added logic to labels

--- ColorizedApp2/ViewController.swift
+++ ColorizedApp2/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK -  IB Outlets
+    @IBOutlet weak var valueRedLabel: UILabel!
+    @IBOutlet weak var valueGreenLabel: UILabel!
+    @IBOutlet weak var valueBlueLabel: UILabel!
+    @IBOutlet weak var colorView: UIView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        valueRedLabel.text = String(redSlider.value)
+        valueGreenLabel.text = String(greenSlider.value)
+        valueBlueLabel.text = String(blueSlider.value)
     }
-
+    
+    // MARK -IB Actoins
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        switch sender.tag {
+        case 1:
+            valueRedLabel.text = String(format: "%.1f", redSlider.value)
+        case 2:
+            valueGreenLabel.text = String(format: "%.1f", greenSlider.value)
+        default:
+            valueBlueLabel.text = String(format: "%.1f", blueSlider.value)
+        }
+    }
+    
 
 }
-
