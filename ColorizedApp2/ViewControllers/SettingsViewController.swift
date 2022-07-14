00822b183fe3a79b5a19cc235b0e635c4c66850a
Oksenoyt <a.oksenoyt@gmail.com>
2022-07-14 12:25:17 +0300

    added textField and logic for them

--- ColorizedApp2/ViewControllers/SettingsViewController.swift
+++ ColorizedApp2/ViewControllers/SettingsViewController.swift
@@ -10,56 +10,62 @@ import UIKit
 class SettingsViewController: UIViewController {
     
     // MARK -  IB Outlets
+    @IBOutlet weak var colorView: UIView!
+    
     @IBOutlet weak var valueRedLabel: UILabel!
     @IBOutlet weak var valueGreenLabel: UILabel!
     @IBOutlet weak var valueBlueLabel: UILabel!
-    
-    @IBOutlet weak var colorView: UIView!
     
     @IBOutlet weak var redSlider: UISlider!
     @IBOutlet weak var greenSlider: UISlider!
     @IBOutlet weak var blueSlider: UISlider!
     
+    @IBOutlet weak var redTextField: UITextField!
+    @IBOutlet weak var greenTextField: UITextField!
+    @IBOutlet weak var blueTextField: UITextField!
+    
     // MARK -  Public Properties
     var color: UIColor!
     var delegate: MainViewControllerDelegate!
     
+    var currentColor: [String: Float] = ["red": 0]
+    
     // MARK -  Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        redTextField.delegate = self
+        greenTextField.delegate = self
+        blueTextField.delegate  = self
+        
         colorView.layer.cornerRadius = 10
-        setSettings()
+        setSettingsFromMainScreen()
     }
     
     // MARK -IB Actoins
     @IBAction func sliderChanged(_ sender: UISlider) {
-        color = UIColor(
-            red: CGFloat(redSlider.value),
-            green: CGFloat(greenSlider.value),
-            blue: CGFloat(blueSlider.value),
-            alpha: 1
-        )
+        getColorFromSliders()
         
         switch sender.tag {
         case 1:
-            valueRedLabel.text = String(format: "%.2f", redSlider.value)
-            colorView.backgroundColor = color
+            currentColor["red"] = redSlider.value
+            setNewSettings()
         case 2:
-            valueGreenLabel.text = String(format: "%.2f", greenSlider.value)
-            colorView.backgroundColor = color
+            currentColor["green"] = greenSlider.value
+            setNewSettings()
         default:
-            valueBlueLabel.text = String(format: "%.2f", blueSlider.value)
-            colorView.backgroundColor = color
+            currentColor["blue"] = blueSlider.value
+            setNewSettings()
         }
     }
     
     @IBAction func closeSettings() {
+        view.endEditing(true)
         delegate.setNewValues(newColor: color)
         dismiss(animated: true)
     }
     
     // MARK - Private methods
-    private func getRGB() -> [String: CGFloat] {
+    private func getRGBFromView() -> [String: Float] {
         var red: CGFloat = 0
         var green: CGFloat = 0
         var blue: CGFloat = 0
@@ -67,28 +73,62 @@ class SettingsViewController: UIViewController {
         
         color.getRed(&red, green: &green, blue: &blue, alpha: &alpha)
         
-        return ["red":red, "green":green, "blue":blue]
+        return ["red": Float(red), "green":Float(green), "blue":Float(blue)]
     }
     
-    private func setSettings() {
-        let currentColorRGB: [String: CGFloat]
-        currentColorRGB = getRGB()
+    private func getColorFromSliders() {
+        color = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    private func setSettingsFromMainScreen() {
+        currentColor = getRGBFromView()
+        setNewSettings()
+    }
+    
+    private func setNewSettings() {
         
-        for (key, value) in currentColorRGB{
+        for (key, value) in currentColor{
             switch key {
             case "red":
-                redSlider.value = Float(value)
+                redSlider.value = value
                 valueRedLabel.text = String(format: "%.2f", value)
+                redTextField.text = String(format: "%.2f", value)
             case "green":
-                greenSlider.value = Float(value)
+                greenSlider.value = value
                 valueGreenLabel.text = String(format: "%.2f", value)
+                greenTextField.text = String(format: "%.2f", value)
             default:
-                blueSlider.value = Float(value)
+                blueSlider.value = value
                 valueBlueLabel.text = String(format: "%.2f", value)
+                blueTextField.text = String(format: "%.2f", value)
             }
         }
+        getColorFromSliders()
         colorView.backgroundColor = color
     }
 }
 
+//MARK: - UITextFildDelegate
+extension SettingsViewController: UITextFieldDelegate {
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        guard let newValue = textField.text else { return }
+        guard let value = Float(newValue) else { return }
+        switch textField.tag {
+        case 1:
+            currentColor["red"] = value
+            setNewSettings()
+        case 2:
+            currentColor["green"] = value
+            setNewSettings()
+        default:
+            currentColor["blue"] = value
+            setNewSettings()
+        }
+    }
+}
 
